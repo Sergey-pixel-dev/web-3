a7ddfbec6dfb136521ee1b5a94ed39d790d94fc9
Sergey-pixel-dev <sonin.serej@yandex.ru>
2024-09-11 18:02:11 +0300

    math was made according to instructions

--- projects/math/main.go
+++ projects/math/main.go
@@ -1,7 +1,14 @@
-package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, world!!!")
+func T(w float64) (t float64) {
+	t = 6 / W(M())
+	return
 }
+func W(m float64) (w float64) {
+	w = math.Sqrt(k / m)
+	return
+}
+func M() (m float64) {
+	m = p * v
+	return
+}
+
